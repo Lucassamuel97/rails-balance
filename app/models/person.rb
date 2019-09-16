d470c7d7b6d2addd10987c4feb1503aafdb0c350
Lucas samuel <lukassamuka88@gmail.com>
2019-09-16 20:12:28 -0300

    Added validations on Person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,4 @@
 class Person < ApplicationRecord
+    validates_presence_of :name
+    validates_numericality_of :age, greater_than: 0, allow_nil: true
 end
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create a simple Person" do
+    assert_difference 'Person.all.count', 1 do
+      Person.create name: 'Person 01'
+    end
+  end
+  test "don't create a Person without name" do
+    assert_difference 'Person.all.count', 0 do
+      Person.create age: 12
+    end
+  end
 end
